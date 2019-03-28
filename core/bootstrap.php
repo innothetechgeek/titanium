7ecfa477d0e50e7f6300ac77ee3f18ff95e779a1
innothetechgeek <innothetechgeek@gmail.com>
2019-03-28 22:15:01 +0200

    My first Commit

---
+++ core/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KhuselaM
+ * Date: 2019-03-28
+ * Time: 22:10
+ */
+echo "hello from bootstrap";
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KhuselaM
+ * Date: 2019-03-28
+ * Time: 21:37
+ */
+session_start();
+define('DS',DIRECTORY_SEPARATOR);
+define('ROOT',dirname(__FILE__));
+$url  = isset($_SERVER['PATH_INFO']) ? explode('/',ltrim($_SERVER['PATH_INFO'],'/')) : [];
+require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
+
