818d74ee8fe35407cf98cf1110e715798e630e5e
Khusela <innothetechgeek@gmail.com>
2020-01-27 12:45:26 +0200

    Added example Controller and Example view

---
+++ app/controllers/Example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KhuselaM
+ * Date: 2020-01-27
+ * Time: 12:13
+ */
+
+class Example extends Controller
+{
+    public function __construct($controller,$action)
+    {
+        parent::__construct($controller,$action);
+    }
+
+    public function index(){
+        $this->view->render('examples/example1');
+    }
+}
---
+++ app/views/examples/example2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KhuselaM
+ * Date: 2020-01-27
+ * Time: 12:04
+ */
