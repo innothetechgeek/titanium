45e4bd2a7fcf9338dc71bbac0a7ee3161d2de52e
Khusela <innothetechgeek@gmail.com>
2020-02-08 19:26:45 +0200

    Added Input class to sanitize user input

---
+++ core/Input.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KhuselaM
+ * Date: 2020-02-08
+ * Time: 19:08
+ */
+
+class Input
+{
+    public static function sanitize($dirty){
+        return htmlentities($dirty,ENT_QUOTES,"UFT-8");
+    }
+
+    public static function get($input){
+        if(isset($_POST[$input])){
+            return self::sanitize($_POST[$input]);
+        }else if(isset($_GET[$input])){
+            return self::sanitize($_GET[$input]);
+        }
+    }
+}
