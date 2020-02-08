8427fe5986445f697b3fd1513fb54e891e9cee34
Khusela <innothetechgeek@gmail.com>
2020-02-08 19:26:02 +0200

    implented redirect method

--- core/Router.php
+++ core/Router.php
@@ -30,4 +30,19 @@ class Router
             die('That method does not exist in the controller '.$controller_name);
         }
     }
+
+    public static function redirect($location){
+        if(!headers_sent()){
+
+            header('location'.ROOT.$location);
+            exit();
+        }else{
+            echo "<script type = 'text/javascript'>";
+            echo "window.location.href='".ROOT.$location."';";
+            echo "</script>";
+            echo "<noscript>";
+            echo '<meta http-equiv="refresh" content="0;url='.$location.'" />';
+            echo "<noscript>";exit;
+        }
+    }
 }
