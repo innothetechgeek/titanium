df68dee802dcc3a15c1c19568826a8abe8d5a102
Khusela <innothetechgeek@gmail.com>
2020-02-15 22:50:06 +0200

    moved login code to the user controller, refactored user Model

---
+++ app/controllers/User.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KhuselaM
+ * Date: 2020-02-08
+ * Time: 01:22
+ */
+
+class User extends Controller
+{
+    public function __construct($controller, $action)
+    {
+        parent::__construct($controller, $action);
+        $this->load_model('Person');
+    }
+
+    public function login(){
+        if($_POST){
+
+           $user = $this->Person->findByUsername(Input::get('usr_name'));
+           dnd($user);
+           if($user){
+
+               $remember = (isset($_POST['remember_me']) && Input::get('remember_me')) ? true : false;
+               $user->login($remember);
+                   //echo "hello";
+                //  Router::redirect('');
+           }
+        }
+        $this->view->render('user/login');
+    }
+
+
+    public function register(){
+
+        $this->view->render('user/login');
+
+    }
+
+    public static function registerUser(){
+
+        $user = new User();
+        $user->save();
+    }
+}
---
+++ app/models/Person.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KhuselaM
+ * Date: 2020-02-08
+ * Time: 16:45
+ */
+
+class Person extends Model
+{
+    private $is_loggedIn, $session_name, $cookieName;
+    public static $currentLoggedInUser = null;
+
+    public function __construct($user = '')
+    {
+
+        $table = 'person';
+        parent::__construct($table);
+        $this->session_name = CURRENT_USER_SESSION_NAME;
+        $this->cookieName = REMEMBER_ME_COOKIE_NAME;
+        $this->soft_delete = true;
+         if ($user != '') {
+             if (is_int($user)) {
+                 $u = $this->db->first('person', ['condition' => 'id = ?', 'bind=' => [$user]]);
+             } else {
+                 $u = $this->db->first('person', ['condition' => 'id = ?', 'bind=' => [$user]]);
+             }
+
+             if ($u) {
+                 foreach ($u as $key => $val) {
+                     $this->$key = $val;
+                 }
+             }
+         }
+    }
+
+    public function findByUsername($username){
+        dnd("the username is $username");
+
+        return $this->find_first(['conditions'=>['usr_name =?'],'bind'=>[$username]]);
+    }
+
+    public function login($rememberMe = false){
+        Session::set($this->session_name,$this->id);
+        if($rememberMe){
+            $hash = md5(uniqid(+rand(0,100)));
+            $user_agent = Session::uagent_no_version();
+            Cookie::set($this->cookieName,$hash,REMEMBER_ME_COOKIE_EXPIRY);
+            $fields = ['session'=>$hash,'user_agent'=>$user_agent,'user_id'=>$this->id];
+            $this->db->query('DELETE FROM user_sessions where usr_id = ? AND usr_agent =?',[$this->id,$user_agent]);
+            $this->db->insert('user_sessions',$fields);
+        }
+    }
+}
