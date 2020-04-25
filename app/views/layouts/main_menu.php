9198e1dd8668b3239dd7c2b58a339c1bafb2afbd
Khusela <innothetechgeek@gmail.com>
2020-04-25 21:23:56 +0200

    minor bug fixes on the menu

--- app/views/layouts/main_menu.php
+++ app/views/layouts/main_menu.php
@@ -9,45 +9,52 @@ $current_page = currentPage();
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <?php foreach ($menu as $key => $val):
-                $active = ''; ?>
-                <?php  if(is_array($val)): ?>
-                    <ul class="navbar-nav mr-auto">
-                        <?php foreach ($val as $k => $v):
-                            $active =  ($v == $current_page) ? 'active' : 'ddddd'; ?>
-                            <?php if($k == 'separator') : ?>
+        <ul class="navbar-nav mr-auto">
 
-                            <?php else : ?>
-                                <li class="nav-item active">
-                                    <a class="nav-link <?=$active?>" href="<?=$k?>"><?=$v?><span class="sr-only">(current)</span></a>
-                                </li>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php else:
-                    $active =  ($key == $current_page) ? 'active' : ''; ?>
-                    <li class="nav-item active">
-                        <a class="nav-link <?=$active?>" href="<?=$key?>"><?=$val?><span class="sr-only">(current)</span></a>
-                    </li>
-                <?php endif; ?>
-        <?php endforeach; ?>
+            <?php
+
+            foreach ($menu as $key => $val):
+                    $active = ''; ?>
+                    <?php  if(is_array($val)):  $i = 0; ?>
+
+                            <?php foreach ($val as $k => $v):
+                                $active =  ($v == $current_page) ? 'active' : 'ddddd'; ?>
+                                <?php if($k == 'separator')  :   ?>
+                                    <div class="dropdown-divider"></div>
+                                <?php else : ?>
+                                    <?php if($i == 0) {?>
+
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <?=$key?>
+                                            </a>
+                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                                    <?php } ?>
+                                            <a class="dropdown-item" href="#"><?=$k?></a>
+                                    <?php if($i == count($val)) { ?>
+
+                                            </div>
+                                        </li>
+
+                                    <?php }?>
+                                <?php endif; ?>
+                            <?php $i++; endforeach; ?>
+
+                    <?php else:
+                        $active =  ($key == $current_page) ? 'active' : ''; ?>
+                        <li class="nav-item active">
+                            <a class="nav-link <?=$active?>" href="<?=$key?>"><?=$key?><span class="sr-only">(current)</span></a>
+                        </li>
+                    <?php endif; ?>
+
+            <?php
+            endforeach; ?>
+        </ul>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             <ul>
-<!--                --><?php //if(currentUser()) : ?>
-<!--                    <li class="nav-item dropdown show">-->
-<!--                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">-->
-<!--                        Hello currentUser()->usr_name;-->
-<!--                    </a>-->
-<!--                    <div class="dropdown-menu show" aria-labelledby="navbarDropdown">-->
-<!--                        <a class="dropdown-item" href="#">Action</a>-->
-<!--                        <a class="dropdown-item" href="#">Another action</a>-->
-<!--                        <div class="dropdown-divider"></div>-->
-<!--                        <a class="dropdown-item" href="#">Something else here</a>-->
-<!--                    </div>-->
-<!--                    </li>-->
-<!--                --><?php //endif; ?>
             </ul>
         </form>
     </div>
