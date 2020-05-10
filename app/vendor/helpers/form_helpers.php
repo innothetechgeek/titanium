f4e3cdc77ab9a991fc263e3d026681b5d629f06f
Khusela <innothetechgeek@gmail.com>
2020-05-10 02:36:38 +0200

    more form helpers

--- app/vendor/helpers/form_helpers.php
+++ app/vendor/helpers/form_helpers.php
@@ -6,11 +6,12 @@
  * Time: 23:07
  */
 
-function inputBlock($type,$label,$name,$name,$value,$input,$input_attribute = [],$div_attributes= []){
-    $attributes = stringfyAttributes($div_attributes);
-    $html = "<div $attributes >";
+function inputBlock($type,$label,$name,$name,$value,$input_attribute = [],$div_attributes= []){
+    $div_attributes = stringfyAttributes($div_attributes);
+    $input_attributes = stringfyAttributes($input_attribute);
+    $html = "<div $div_attributes >";
     $html .= "<label for = '$name'>$label</label>";
-    $html .= "<input type='$type' id = '' value = '$value' name = '$name' $attributes />";
+    $html .= "<input type='$type' id = '' value = '$value' name = '$name' $input_attributes />";
     $html .= "</div>";
 
     return $html;
@@ -22,4 +23,23 @@ function stringfyAttributes($attributes){
         $string .= ' '. $attribute_name . '="'. $attribute_value . '"';
     }
     return $string;
+}
+
+function submitButton($submitText, $input_attributes){
+    $input_string_attr = stringfyAttributes($input_attributes);
+    $html = "<input type = 'submit' value '$submitText'  $input_string_attr />";
+    return $html;
+}
+
+function submitBlock($submitText,$inputAttr = [], $div_attr = []){
+
+    $div_attributes = stringfyAttributes($div_attr);
+    $input_attributes = stringfyAttributes($inputAttr);
+
+    $html = "<div $div_attributes >";
+    $html .= '<input type = "submit" value = "'.$submitText. ''.$input_attributes.'"  />';
+    $html .= "</div>";
+
+    return $html;
+
 }
