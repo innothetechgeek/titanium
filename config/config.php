9e203057cc1d85728fa6004ae189d46bb7f0eccb
Khusela <innothetechgeek@gmail.com>
2020-05-29 00:01:39 +0200

    testing if gitignore actually works

--- config/config.php
+++ config/config.php
@@ -4,13 +4,14 @@
  * User: KhuselaM
  * Date: 2019-03-31
  * Time: 01:04
+ ..........
  */
     define('DEBUG',true);
     define('DB_NAME','titanium');
     define('DB_USER','root');
     define('DB_PASSWORD','');
-    define('DB_HOST','127.0.0.11:3309');
-    define('DEFAULT_CONTROLLER','Home'); //default controller
+    define('DB_HOST','127.0.0.11111:3309');
+    define('DEFAULT_CONTROLLER','Homee'); //default controller
     define('DEFAULT_LAYOUT','default'); //deafult layout
     define('SITE_TITLE', 'TITANIUM MVC Framework');
     define('CURRENT_USER_SESSION_NAME','titADgexYbz1');
