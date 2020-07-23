553cbf108b5691817245c7675f7ed4eb95e9c2f8
Khusela <innothetechgeek@gmail.com>
2020-07-23 02:23:24 +0200

    empty space

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,6 +5,9 @@
  * Date: 2019-04-21
  * Time: 16:46
  */
+ namespace app\controllers;
+ use core\Controller;
+ use app\models\Genre;
 
 class Home extends Controller
 {
@@ -15,6 +18,11 @@ class Home extends Controller
 
     public function index(){
 
+        $genres = \app\models\Genre::where('gnr_name',"Romance")->get();
+        dnd($person);
+      //  $person::where('usr_name',"Jessica Anthony")->get();
+      // dnd($person);
+      //  $person->save();
         $this->view->render('index');
 
     }
